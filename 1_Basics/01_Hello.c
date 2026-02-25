d825ade7042231a212f4e2bf7728a49c1213d37e
Tristian Geoffrey <geofreypembwa001@gmail.com>
2026-02-25 17:48:34 +0300

    Add Hello World program in C

---
+++ 1_Basics/01_Hello.c
@@ -0,0 +1,7 @@
+// Header file for input output functions
+#include <stdio.h>
+// Main function: entry point for execution
+int main() {
+    printf("Hello World"); //Prints hello world 
+    return 0;
+}
